68287258ccabbc4830be9fe0a244f24f0bba2207
jianhui <liujh1@fosun.com>
2023-08-25 10:56:17 +0800

    feat: doc-cli 支持多入口

--- packages/doc-cli/src/bin/index.ts
+++ packages/doc-cli/src/bin/index.ts
@@ -1,12 +1,13 @@
-// import { fileURLToPath } from 'url'
+import path from 'node:path'
 import process from 'node:process'
+import { fileURLToPath } from 'node:url'
 import { createServer } from 'vite'
 import Inspect from 'vite-plugin-inspect'
 import react from '@vitejs/plugin-react'
 import vue from '@vitejs/plugin-vue'
 import mdPlugin from '../plugin/md-plugin'
 
-// const __dirname = fileURLToPath(new URL('.', import.meta.url))
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 ;
 
@@ -17,6 +18,14 @@ import mdPlugin from '../plugin/md-plugin'
     root: process.cwd(),
     server: {
       port: 1337,
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, '..', '..', 'index.html'),
+          react: path.resolve(__dirname, '..', '..', 'react/index.html'),
+        },
+      },
     },
     plugins: [
       mdPlugin(),
